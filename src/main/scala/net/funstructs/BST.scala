fb01fdad7d55ebc01932d31380a2ce98b83c3e53
Jeff Stokes <jeffecu88@gmail.com>
2014-07-07 22:50:24 -0400

    Flesh out BSTs and add some specs

--- src/main/scala/net/funstructs/BST.scala
+++ src/main/scala/net/funstructs/BST.scala
@@ -1,14 +1,46 @@
 package net.funstructs
 
+import scala.collection.generic.CanBuildFrom
+
 /**
  * Created by jeff on 7/5/14.
  */
-sealed abstract class BST[+A] {
+sealed abstract class BST[+A <% Ordered[A]] {
+  def member[X >: A <% Ordered[X]](e: X): Boolean
+  def insert[X >: A <% Ordered[X]](e: X): BST[X]
+  def map[B](f: A => B): List[B]
 }
 
-case class Node[+A](value: A, left: BST[A], right: BST[A]) extends BST[A] {
-  override def toString = s"($left, $value, $right)"
-}
+// Empty nodes
 case object E extends BST[Nothing] {
+
   override def toString = "(empty)"
+
+  override def member[X <% Ordered[X]](e: X): Boolean = false
+
+  override def insert[X <% Ordered[X]](e: X): BST[X] = Node[X](e, left = E, right = E)
+
+  override def map[B](f: (Nothing) => B): List[B] = List[B]()
+}
+
+
+// Populated...
+case class Node[+A <% Ordered[A]](value: A, left: BST[A], right: BST[A]) extends BST[A] {
+
+  override def toString = s"($left, $value, $right)"
+
+  override def member[X >: A <% Ordered[X]](e: X): Boolean = e match {
+    case x if x < value => left.member(x)
+    case x if x > value => right.member(x)
+    case _ => true // ==
+  }
+
+  override def insert[X >: A <% Ordered[X]](e: X): BST[X] = e match {
+    case x if x < value => Node[X](value, left = left.insert(e), right)
+    case x if x > value => Node[X](value, left, right = right.insert(e))
+    case _ => this // ==
+  }
+
+  // TODO not the best impl of map, slow and inefficient
+  override def map[B](f: A => B): List[B] = f(value) :: left.map(f) ::: right.map(f)
 }
--- src/test/scala/net/funstructs/BSTSpec.scala
+++ src/test/scala/net/funstructs/BSTSpec.scala
@@ -10,5 +10,28 @@ import Prop._
  */
 object BSTSpec extends Properties("Stack") {
 
+  type IntTree = BST[Int]
 
+  lazy val genBST: Gen[IntTree] = for {
+    n <- arbitrary[Int]
+    s <- oneOf(const(E), genBST)
+  } yield s.insert(n)
+
+  implicit def arbBST = Arbitrary(genBST)
+
+  property("have smaller keys on the left, larger on the right") = forAll { tree: IntTree =>
+    def check(tree: IntTree): Boolean = tree match {
+      case E => true
+      case Node(_, E, E) => true
+      case Node(x, E, r: Node[Int]) if x < r.value => check(r)
+      case Node(x, l: Node[Int], E) if x > l.value => check(l)
+      case Node(x, l: Node[Int], r: Node[Int]) if x > l.value && x < r.value => check(l) && check(r)
+      case _ => false
+    }
+    check(tree)
+  }
+  property("contain no duplicate entries") = forAll { tree: IntTree =>
+    val values = tree map identity
+    values.toSet.size == values.size
+  }
 }
