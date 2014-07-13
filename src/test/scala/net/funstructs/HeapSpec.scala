dc0503ba0616caf7bc05065407fe68699a1a5136
Jeff Stokes <jeffecu88@gmail.com>
2014-07-13 12:03:52 -0400

    Add placeholder spec for heaps

---
+++ src/test/scala/net/funstructs/HeapSpec.scala
@@ -0,0 +1,13 @@
+package net.funstructs
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+/**
+ * Created by jeff on 7/13/14.
+ */
+object HeapSpec extends Properties("Stack") {
+
+}
